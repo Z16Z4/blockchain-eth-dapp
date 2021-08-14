359e186985e1391adc8071a51795de76c4aa3081
cronos-hash <apollo@dnmx.org>
2021-08-14 03:26:39 +0100

    connect to blockchain (metamask)

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,12 +2,27 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     uint public taskCount = 0;
+
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+
+    }
     //asynchronous action using:
     //todoList = await TodoList.deployed()
     //wait for this result then assign to variable
-    //todoList.address => 0xfa8a061a772BE4938B2a2B3737B4668F9Cf9c0de
     //todoList.taskCount() => 0
     //taskCount = await todoList.taskCount()
     //taskCount.toNumber() => gets as num
     //checks to make everything is setup properly
+    mapping(uint => Task) public tasks;
+    constructor() public {
+        createTask("invest in monero instead");
+    }
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
